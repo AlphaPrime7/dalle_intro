5a04775fff9dc1cc686169754c1b1bb82002bd63
alphaprime7 <awesome.tingwei@outlook.com>
2023-09-24 12:39:31 -0500

    up

--- R/dalle_demo.R
+++ R/dalle_demo.R
@@ -10,7 +10,8 @@ library(openai)
 library(gptstudio)
 library(tidyverse)
 
-#prompt dalle
+#prompt dalle-Image in the project folder-NOT BAD
+#fun fact- I really dont know what dalle does? Can i feed it screenshot?
 prompt_dalle <- "create a high quality background of greek gods with multiple sky colors"
 demo <- create_image(prompt_dalle)
 demo_url <- demo$data$url
